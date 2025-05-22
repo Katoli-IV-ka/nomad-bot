3864cbb709e7b2b09df7e1edf38130ebde6d0468
MaxKh <MaxKhvedinich@gmail.com>
2025-05-22 18:27:53 +0300

    add: change request contact

--- contents/booking/share_contact_contents.py
+++ contents/booking/share_contact_contents.py
@@ -6,4 +6,8 @@ share_contact_photo = "AgACAgIAAxkBAAINI2grgiJaxKsaTrNzLj7Yab0FtoWPAAKj6jEbiftgS
 def share_contact_keyboard() -> ReplyKeyboardMarkup:
     keyboard = ReplyKeyboardBuilder()
     keyboard.button(text="Отправить свой контакт", request_contact=True)
-    return keyboard.as_markup(one_time_keyboard=True)
+    return keyboard.as_markup(
+        one_time_keyboard=True,
+        resize_keyboard=False,
+        is_persistent=True,
+    )
